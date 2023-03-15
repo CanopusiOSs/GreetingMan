cdcfa1417777406976a3c9cbdbf7fc1bd4116732
Nirmal <nirmal.patel@canopusinfosystems.com>
2023-03-15 21:46:13 +0530

    intial commit

---
+++ GreetingMan/GreetingMan/GreetingMan.h
@@ -0,0 +1,18 @@
+//
+//  GreetingMan.h
+//  GreetingMan
+//
+//  Created by IOS on 15/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GreetingMan.
+FOUNDATION_EXPORT double GreetingManVersionNumber;
+
+//! Project version string for GreetingMan.
+FOUNDATION_EXPORT const unsigned char GreetingManVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GreetingMan/PublicHeader.h>
+
+
---
+++ GreetingMan/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  GreetingMan
+//
+//  Created by IOS on 15/03/23.
+//
+
+import Foundation
+
+public class Service {
+    public init() {}
+    public func doSomething() -> String {
+        return "Hello young man"
+    }
+}
